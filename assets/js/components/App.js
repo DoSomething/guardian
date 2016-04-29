e8f880cd9394c8ca0568873d06f1d940fbebc618
Aaron Schachter <aaronschachter@gmail.com>
2016-04-29 16:03:12 -0700

    Refactor as RegisterForm.js, login upon register

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
-
-import LoginForm from './LoginForm';
-import NavLink from './NavLink';
 import activeComponent from 'react-router-active-component';
+
+import RegisterForm from './RegisterForm';
+import NavLink from './NavLink';
+import Helpers from '../utils/Helpers.js';
+
 let NavItem = activeComponent('li');
 
 export default React.createClass({
   authenticateUser: function(credentials) {
+    this.firebaseRef.authWithPassword(credentials, function(error, userData) {
+      if (error) {
+        console.log("Error logging in user:", error);
+      } 
+      else {
+        console.log("Successfully logged in user account with uid:", userData.uid);
+      }
+    });
     this.setState({
       email: credentials.email
+    });
+  },
+  componentWillMount: function() {
+    this.firebaseRef = new Firebase(Helpers.firebaseUrl());
+    var authData = this.firebaseRef.getAuth();
+    if (authData) {
+      this.setState({
+        email: authData.password.email
+      });
+    }
+  },
+  createUser: function(credentials) {
+    var self = this;
+    this.firebaseRef.createUser(credentials, function(error, userData) {
+      if (error) {
+        switch (error.code) {
+          case "EMAIL_TAKEN":
+            console.log("The new user account cannot be created because the email is already in use.");
+            break;
+          case "INVALID_EMAIL":
+            console.log("The specified email is not a valid email.");
+            break;
+          default:
+            console.log("Error creating user:", error);
+        }
+      } else {
+        console.log(this);
+        self.authenticateUser(credentials);
+      }
     });
   },
   getInitialState() {
@@ -17,13 +56,19 @@ export default React.createClass({
     }
   },
   logoutUser: function() {
+    this.firebaseRef.unauth();
     this.setState({
       email: null
     });
   },
   render() {
+    var content = this.props.children;
     if (!this.state.email) {
-      return <AnonView authenticateUser={this.authenticateUser} />;
+      content = (
+        <AnonView 
+          authenticateUser={this.authenticateUser} 
+          createUser={this.createUser} />
+      );
     }
     return (
       <div>
@@ -32,7 +77,7 @@ export default React.createClass({
           logoutUser={this.logoutUser} 
         />
         <div className="container">
-          {this.props.children}
+          {content}
         </div>
       </div>
     )
@@ -44,11 +89,12 @@ var AnonView = React.createClass({
     return (
       <div className="container">
         <div className="page-header">
-          <img src="/images/ds-blue.png" className="center-block img-circle" />
+          <img src="/images/logo.png" className="center-block" />
+          <h1 className="text-center">Register</h1>
         </div>
         <div className="row">
           <div className="col-md-4 col-md-offset-4">
-            <LoginForm authenticateUser={this.props.authenticateUser} />
+            <RegisterForm createUser={this.props.createUser} />
           </div>
         </div>
       </div>
@@ -56,8 +102,16 @@ var AnonView = React.createClass({
   }
 })
 
-var Navbar =  React.createClass({
+var Navbar = React.createClass({
   render: function() {
+    var content = <NavBarLoginForm />;
+    if (this.props.email) {
+      content = (
+        <NavBarAuthenticated 
+          email={this.props.email}
+          logoutUser={this.props.logoutUser} />
+      );
+    }
     return (
       <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container">
@@ -73,25 +127,52 @@ var Navbar =  React.createClass({
             </a>
           </div>
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-              <NavItem to="/campaigns">Campaigns</NavItem>
-              <NavItem to="/members">Members</NavItem>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                  <small>{this.props.email}</small> <span className="caret"></span>
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Reviews</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a onClick={this.props.logoutUser}><small>Logout</small></a></li>
-                </ul>
-              </li>
-            </ul>
+          {content}
           </div>
         </div>
       </nav>
     );
+  }
+});
+
+var NavBarAuthenticated = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ul className="nav navbar-nav">
+          <NavItem to="/campaigns">Campaigns</NavItem>
+          <NavItem to="/members">Members</NavItem>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li className="dropdown">
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              <small>{this.props.email}</small> <span className="caret"></span>
+            </a>
+            <ul className="dropdown-menu">
+              <li><a href="#">Reviews</a></li>
+              <li role="separator" className="divider"></li>
+              <li><a onClick={this.props.logoutUser}><small>Logout</small></a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+var NavBarLoginForm = React.createClass({
+  render: function() {
+    return (
+      <form className="navbar-form navbar-right" role="search">
+        <div className="form-group">
+          <input type="text" className="form-control" name="username" placeholder="psloth@dosomething.org" />
+        </div>
+        <div className="form-group">
+          <input type="text" className="form-control" name="password" placeholder="Password" />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    );
+
   }
 });
