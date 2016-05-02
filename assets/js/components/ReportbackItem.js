e908ba6565c144cc93deb1fe66d41e4fd7d2ee6b
Aaron Schachter <aaronschachter@gmail.com>
2016-05-02 15:05:52 -0700

    Writes media.gallery to Firebase, campaigns/:campaign/media/gallery/:media

--- assets/js/components/ReportbackItem.js
+++ assets/js/components/ReportbackItem.js
@@ -10,36 +10,35 @@ export default React.createClass({
     var firebaseRef = new Firebase(Helpers.firebaseUrl());
     this.bindAsObject(firebaseRef.child("media/" + this.props.mediaId), "media");
   },
+  handleClick: function() {
+    var saveValue = !this.state.media.gallery;
+    this.firebaseRefs.media.child("gallery").set(saveValue);
+    var firebaseRef = new Firebase(Helpers.firebaseUrl());
+    var galleryUrl = "campaigns/" + this.state.media.campaign + "/media/gallery/" + this.props.mediaId
+    firebaseRef.child(galleryUrl).set(saveValue);
+  },
   mixins: [ReactFireMixin],
   render: function() {
     if (!this.state) {
       return null;
     }
-    var reportbackItem = this.renderReportbackItem();
+    var status = null;
+    if (this.state.media.gallery) {
+      status = <small><ReportbackStatusIcon status={"approved"} /> gallery</small>;
+    }
+    else {
+      status = <small><ReportbackStatusIcon status={"excluded"} /> excluded</small>;
+    }
     return (
-      <div className="reportback-item">
-        <button disabled={!this.props.reviewing}>
-        <img src={this.state.media.uri} />
-        <ul className="nav nav-pills">
-          <li>
-            <ReportbackStatusIcon status={"approved"} /> gallery
-          </li>
-        </ul>
-        <h4 className="text-center">{this.state.media.caption}</h4>
+      <div className="reportback-item" id={"media-" + this.props.mediaId} >
+        <button disabled={!this.props.reviewing} onClick={this.handleClick}>
+          <img src={this.state.media.uri} />
+          <ul className="nav nav-pills">
+            <li>{status}</li>
+          </ul>
+          <h4 className="text-center">{this.state.media.caption}</h4>
         </button>
       </div>
     );
-  },
-  renderReportbackItem: function() {
-    return 
-      <div>
-        <img src={this.state.media.uri} />
-        <ul className="nav nav-pills">
-          <li>
-            <ReportbackStatusIcon status={"approved"} /> gallery
-          </li>
-        </ul>
-        <h4 className="text-center">{this.state.media.caption}</h4>
-      </div>;
-  },    
+  }, 
 })
